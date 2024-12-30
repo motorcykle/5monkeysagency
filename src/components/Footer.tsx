c109fdc28ecb1acf06908a9c3afc370b47a141e6
Modi Yousef <51007395+motorcykle@users.noreply.github.com>
2024-12-30 02:51:30 +0100

    footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <footer className="w-screen py-4 text-black">
+      <div className="container mx-auto max-w-7xl flex flex-col items-center font-sans justify-between gap-4 px-4 md:flex-row">
+        <p className="text-center  text-sm font-light md:text-left">
+          motorcykle 2024. All rights reserved
+        </p>
+
+        <a
+          href="#privacy-policy"
+          className="text-center text-sm font-light hover:underline md:text-right"
+        >
+          Privacy Policy
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
